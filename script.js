9eee7fa581d5864196b16045e06c99e9eeda02a6
Adetuyi <seyispecial17@gmail.com>
2022-05-18 08:58:55 +0100

    Updates

---
+++ script.js
@@ -0,0 +1,34 @@
+// DOM Elements
+const questions = document.querySelectorAll('.q');
+const icons = document.querySelectorAll('.icons img');
+const animators = document.querySelectorAll('.will-animate');
+const callback = (entries, observer) => {
+	entries.forEach((entry) => {
+		if (!entry.isIntersecting) {
+			return;
+		}
+		entry.target.classList.add('fadeIn');
+		observer.unobserve(entry.target);
+	});
+};
+
+const observer = new IntersectionObserver(callback, {
+	root: null,
+	threshold: 1,
+	rootMargin: '0px 0px -100px 0px',
+});
+
+// Event listeners
+questions.forEach((question) => {
+	question.addEventListener('click', (e) => {
+		e.currentTarget.parentElement.classList.toggle('collapsed');
+	});
+});
+
+icons.forEach((icon) => {
+	icon.addEventListener('click', (e) => {
+		e.currentTarget.parentElement.classList.toggle('open');
+	});
+});
+
+animators.forEach((animator) => observer.observe(animator));
